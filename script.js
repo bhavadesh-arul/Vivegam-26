6c6d60705be4d7a8b6635ead796c937bc478d275
bhavadesh-arul <bhavadesharul@gmail.com>
2026-02-07 09:39:13 +0530

    Added stylish design and animation

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 document.querySelectorAll(".links a").forEach(btn => {
   btn.addEventListener("click", e => {
-    const ripple = document.createElement("span");
 
+    btn.style.boxShadow =
+      "0 0 40px rgba(120,160,255,0.9), 0 0 80px rgba(120,160,255,0.6)";
+
+    setTimeout(() => {
+      btn.style.boxShadow = "";
+    }, 300);
+
+    const ripple = document.createElement("span");
     ripple.style.position = "absolute";
     ripple.style.width = ripple.style.height = "120px";
-    ripple.style.background = "rgba(255,255,255,0.4)";
+    ripple.style.background = "rgba(120,160,255,0.5)";
     ripple.style.borderRadius = "50%";
     ripple.style.left = `${e.offsetX - 60}px`;
     ripple.style.top = `${e.offsetY - 60}px`;
